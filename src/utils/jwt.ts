c5dbe9853da1e685d54951ceff0b10a7604043d3
Rakib <hellorakibhossen@gmail.com>
2025-07-06 16:04:54 +0600

    type error fix

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,8 +1,20 @@
-import jwt from "jsonwebtoken";
-import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
+// import jwt from "jsonwebtoken";
+// import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
 
-export const generateToken = (payload: object): string => {
-  return jwt.sign(payload, JWT_SECRET as string, {
-    expiresIn: JWT_EXPIRES_IN,
-  });
+// export const generateToken = (payload: object): string => {
+//   return jwt.sign(payload, JWT_SECRET as string, {
+//     expiresIn: JWT_EXPIRES_IN,
+//   });
+// };
+
+import jwt from "jsonwebtoken";
+
+export const generateToken = (payload: object) => {
+  const secret = process.env.JWT_SECRET as string;
+
+  if (!secret) {
+    throw new Error("JWT_SECRET is not defined");
+  }
+
+  return jwt.sign(payload, secret, { expiresIn: "7d" });
 };
