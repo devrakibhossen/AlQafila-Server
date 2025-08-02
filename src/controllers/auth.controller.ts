75f235cf2d7485098de9efa0e658a9d219273264
Rakib <hellorakibhossen@gmail.com>
2025-08-02 12:12:00 +0600

    some error fix

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
-// import jwt from "jsonwebtoken";
 // import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
 import User from "../models/user.model.js";
 import { NextFunction, Request, Response } from "express";
